8eaf983a0e89b9f1adcb09dd4a415a79eac97b29
xliuchao <1423639404@qq.com>
2016-07-05 17:33:27 +0800

    add 语音合成 语音识别

--- MyApplication/app/src/main/java/com/example/admin/xunfeivoice/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/admin/xunfeivoice/MainActivity.java
@@ -3,24 +3,41 @@ package com.example.admin.xunfeivoice;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.tv_tts)
+    TextView tts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        findViewById(R.id.tv_iat).setOnClickListener(this);
     }
-    //语音听写
-    @OnClick(R.id.tv_iat)
+
+    @OnClick({R.id.tv_iat, R.id.tv_tts,R.id.tv_isr})
     @Override
-    public void onClick(View view){
-         startActivity(new Intent(this,IatDemo.class));
+    public void onClick(View view) {
+        Log.e("print","tts:"+tts.toString());
+        switch (view.getId()) {
+            //语音听写
+            case R.id.tv_iat:
+                startActivity(new Intent(this, IatDemo.class));
+                break;
+            //语音合成
+            case R.id.tv_tts:
+                startActivity(new Intent(this, TtsDemo.class));
+                break;
+            case R.id.tv_isr:
+                startActivity(new Intent(this, AsrDemo.class));
+                break;
+        }
     }
 }
